d74424d217dd5b46820ca3dad0f303e2421e347b
br6dsk <44901467+br6dsk@users.noreply.github.com>
2020-07-14 21:38:38 -0300

    Update thumb.py

--- thumb.py
+++ thumb.py
@@ -23,8 +23,8 @@ banner = """
 
 ► phpThumb version <= 1.7.9 (RCE)
 ► Automatic file upload
-► Coded 4Study by: Brndsk
-► Github: github.com/brndsk
+► Coded 4Study by: Br6dsk
+► Github: github.com/br6dsk
 
 ► Use: xpl.py --url http:/vuln.com/phpThumb/
 ► Use: xpl.py --list list.txt
